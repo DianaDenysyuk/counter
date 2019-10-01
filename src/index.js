a3bdf25e71b6b4e720c30bec98a52dce5c392eb4
Діана Денисюк <denys_d@rivne.itstep.org>
2019-10-01 11:57:43 +0300

    add easy counter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const inititalState = 0;
+const countReducer = (state = inititalState, action) =>{
+    switch (action.type) {
+        case "INC":
+            return state +1;
+        case "DEC":
+            return state -1;
+        case "plustwo":
+            return state +2;
+        case "zero":
+            return state = 0;
+        case "multiplatetwo":
+            return state *2;
+        case "dividetwo":
+            return state /2;
+        default:
+            return state
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(countReducer);
+
+store.subscribe(() =>{
+    document.getElementById('count').innerHTML = store.getState();
+})
+document.getElementById('plus').addEventListener("click", () =>{
+    store.dispatch({type: 'INC'});
+});
+document.getElementById('minus').addEventListener("click", () =>{
+    store.dispatch({type: 'DEC'});
+});
+document.getElementById('plustwo').addEventListener("click", () =>{
+    store.dispatch({type: 'plustwo'});
+});         
+document.getElementById('zero').addEventListener("click", () =>{
+    store.dispatch({type: 'zero'});
+});
+document.getElementById('multiplatetwo').addEventListener("click", () =>{
+    store.dispatch({type: 'multiplatetwo'});
+});
+document.getElementById('dividetwo').addEventListener("click", () =>{
+    store.dispatch({type: 'dividetwo'});
+});
